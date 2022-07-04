077c55916f41335fd5e2f1af229af2a2417fe12f
Anderson Miyake <afmyk@hotmail.com>
2022-07-04 20:33:33 -0300

    Creating the Position Class

---
+++ Chess/Board/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Board
+{
+    internal class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position()
+        {
+        }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Row + ", " + Column;
+        }
+    }
+}
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Board;
 
 namespace Chess
 {
@@ -6,7 +7,12 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position p;
+
+            p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
+
         }
     }
 }
